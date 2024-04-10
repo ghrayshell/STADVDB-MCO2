6d9ccf0bcfdc18756419ef1df6c5c50d40505317
MiguelCarloKua <miguel_carlo_kua@dlsu.edu.ph>
2024-04-10 16:35:09 +0800

    initial commit

---
+++ controller/controller.js
@@ -0,0 +1,27 @@
+const {node1, node2, node3, node_utils} = require('../model/nodes');
+const dbquery = require('../model/database');
+const dotenv = require(`dotenv`).config();
+
+const controller = {
+    //get all the data from the database here
+    getIndex: async function (req, res) {
+        if(await node_utils.pingNode(1)){
+            console.log("CONNECTED NODE 1");
+        }
+        else{
+            console.log("NODE 1 down");
+        }
+
+        if(await node_utils.pingNode(2)){
+            console.log("CONNECTED NODE 2");
+        }
+        else{
+            console.log("NODE 2 down");
+        }
+        const appointments = await dbquery.selectQuery();
+                
+        res.render('index', {appointments})
+    }
+}
+
+module.exports = controller;
